eaa35b3eb780b860e3bcde7ece1172bb26251c5e
xiangflight <xiangflight@foxmail.com>
2019-03-14 09:18:23 +0800

    update solution34 - 35

---
+++ src/coding_interviews/Solution35.java
@@ -0,0 +1,26 @@
+package coding_interviews;
+
+/**
+ * @author 赵翔 xiangflightATfoxmaildotcom
+ * @version coding_interviews
+ * @date 2019-03-13 22:52
+ *
+ * 从1到n整数中1出现的次数
+ *
+ */
+
+public class Solution35 {
+
+    /**
+     * 题目：输入一个整数n，求1到n这n个整数的十进制表示中
+     *      1出现的次数。例如输入12，从1-12这些整数中包含
+     *      1的数字有1，10，11，12，1共出现5次。
+     *
+     * @param n 输入的n
+     * @return 1出现的次数
+     */
+    public int numberOf1Between1AndN(int n) {
+        return 0;
+    }
+
+}
