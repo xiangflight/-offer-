0bce044d8b446b4b3d50a58c494beebb03c8ddaf
xiangflight <xiangflight@foxmail.com>
2019-03-23 09:45:54 +0800

    update solution 44 add annotation

--- src/coding_interviews/Solution44.java
+++ src/coding_interviews/Solution44.java
@@ -26,10 +26,20 @@ public class Solution44 {
             num2[0] = 0;
             return;
         }
+        // 为什么allXor初始化0，因为0与任何数异或都等于数本身
         int allXor = 0;
         for (int val: array) {
             allXor ^= val;
         }
+        // 此时，allXor就是两个出现一次数字的异或结果
+        // ..
+        // 有两种方式可以求得allXor最右边的1
+        // 1. 通过遍历数的二进制方式，每一位与1与
+        // 2. 与负的allXor做与操作，可以迅速得到allXor最右的一位
+        //    举例：
+        //     12 二进制表示    1100
+        //    -12 二进制表示    0100 (正数二进制每一位取反加一)
+        //            与结果   0100
         allXor &= -allXor;
         for (int val: array) {
             if ((val & allXor) == 0) {
