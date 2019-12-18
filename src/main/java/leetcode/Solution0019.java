80cd9298f8f84b8347d9c28e359ccc6b822b59eb
xiangflight <xiangflight@foxmail.com>
2019-12-18 09:01:02 +0800

    feat(leetcode): add more solutions

---
+++ src/main/java/leetcode/Solution0019.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+import ds.linkedlist.ListNode;
+
+/**
+ * @author xiangdotzhaoAtwoqutechcommacom
+ * @date 2019/12/18
+ */
+
+public class Solution0019 {
+
+    /**
+     * 保持一个固定大小的窗口
+     *
+     * @param head 头节点
+     * @param n 倒数第 n 个
+     * @return 返回删除后的链表
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode p = dummy;
+        ListNode q = dummy;
+        while (n-- >= 0) {
+            q = q.next;
+        }
+        while (q != null) {
+            p = p.next;
+            q = q.next;
+        }
+        p.next = p.next.next;
+        return dummy.next;
+    }
+
+}
