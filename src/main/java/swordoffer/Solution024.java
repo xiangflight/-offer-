1550aec39b0bf95d2d6d4ba7faf2b4524585caac
xiangflight <xiangflight@foxmail.com>
2020-01-01 17:30:11 +0800

    feat(swordoffer): solution 24

---
+++ src/main/java/swordoffer/Solution024.java
@@ -0,0 +1,51 @@
+package swordoffer;
+
+import ds.linkedlist.ListNode;
+
+/**
+ * @author xiangdotzhaoAtwoqutechcommacom
+ * @date 2020/1/1
+ */
+
+public class Solution024 {
+
+    public ListNode ReverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode curr = head;
+        ListNode pre = null;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+
+    public ListNode ReverseList2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = null;
+        ListNode reverseHead = ReverseList2(next);
+        next.next = head;
+        return reverseHead;
+    }
+
+    public ListNode ReverseList3(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = dummy.next;
+            dummy.next = head;
+            head = next;
+        }
+
+        return dummy.next;
+    }
+
+}
