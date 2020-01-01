42d8675d7441a0327ffadc697a92d0c7e3b61e6b
xiangflight <xiangflight@foxmail.com>
2020-01-01 10:02:43 +0800

    feat(leetcode): add more solutions

--- src/main/java/swordoffer/Solution018.java
+++ src/main/java/swordoffer/Solution018.java
@@ -5,6 +5,9 @@ import ds.linkedlist.ListNode;
 /**
  * @author xiangdotzhaoAtwoqutechcommacom
  * @date 2020/1/1
+ * <p>
+ * 在一个排序的链表中，存在重复的结点，请删除该链表中重复的结点，重复的结点不保留，返回链表头指针。
+ * 例如，链表1->2->3->3->4->4->5 处理后为 1->2->5
  */
 
 public class Solution018 {
@@ -29,6 +32,44 @@ public class Solution018 {
             }
         }
         return head;
+    }
+
+    public ListNode deleteDuplication(ListNode pHead) {
+        if (pHead == null || pHead.next == null) {
+            return pHead;
+        }
+        ListNode dummy = new ListNode(-1);
+        dummy.next = pHead;
+        ListNode pre = dummy;
+        ListNode cur = pHead;
+        while (cur != null) {
+            while (cur.next != null && cur.next.val == cur.val) {
+                cur = cur.next;
+            }
+            if (pre.next == cur) {
+                pre = pre.next;
+            } else {
+                pre.next = cur.next;
+            }
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+
+    public ListNode deleteDuplication2(ListNode pHead) {
+        if (pHead == null || pHead.next == null) {
+            return pHead;
+        }
+        ListNode next = pHead.next;
+        if (pHead.val == next.val) {
+            while (next != null && pHead.val == next.val) {
+                next = next.next;
+            }
+            return deleteDuplication2(next);
+        } else {
+            pHead.next = deleteDuplication2(pHead.next);
+            return pHead;
+        }
     }
 
 }
