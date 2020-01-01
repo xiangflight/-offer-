05012bb74aea1b7658fa6a050a01690f55884571
xiangflight <xiangflight@foxmail.com>
2020-01-01 19:28:16 +0800

    feat(swordoffer): solution 28

---
+++ src/main/java/swordoffer/Solution028.java
@@ -0,0 +1,32 @@
+package swordoffer;
+
+import ds.tree.TreeNode;
+
+/**
+ * @author xiangdotzhaoAtwoqutechcommacom
+ * @date 2020/1/1
+ */
+
+public class Solution028 {
+
+    boolean isSymmetrical(TreeNode pRoot) {
+        if (pRoot == null) {
+            return true;
+        }
+        return isSymmetrical(pRoot.left, pRoot.right);
+    }
+
+    private boolean isSymmetrical(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return isSymmetrical(left.left, right.right) && isSymmetrical(left.right, right.left);
+    }
+
+}
