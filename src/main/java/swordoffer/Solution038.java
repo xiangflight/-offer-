946757e1116dd33942fd863168cf0e958d66304d
xiangflight <xiangflight@foxmail.com>
2020-01-02 09:29:55 +0800

    feat(swordoffer): solution 38

--- src/main/java/swordoffer/Solution038.java
+++ src/main/java/swordoffer/Solution038.java
@@ -9,6 +9,8 @@ import java.util.Arrays;
  * <p>
  * 输入一个字符串,按字典序打印出该字符串中字符的所有排列。
  * 例如输入字符串abc,则打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。
+ * <p>
+ * permutation and combination
  */
 
 public class Solution038 {
