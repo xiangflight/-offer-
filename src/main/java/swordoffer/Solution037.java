ba621bdb9fa3811317c066b829406f6f70430925
xiangflight <xiangflight@foxmail.com>
2020-01-02 09:09:13 +0800

    feat(swordoffer): solution 37

---
+++ src/main/java/swordoffer/Solution037.java
@@ -0,0 +1,63 @@
+package swordoffer;
+
+import ds.tree.TreeNode;
+
+/**
+ * @author xiangdotzhaoAtwoqutechcommacom
+ * @date 2020/1/1
+ * <p>
+ * 请实现两个函数，分别用来序列化和反序列化二叉树
+ * <p>
+ * 二叉树的序列化是指：把一棵二叉树按照某种遍历方式的结果以某种格式保存为字符串，从而使得内存中建立起来的二叉树可以持久保存。
+ * 序列化可以基于先序、中序、后序、层序的二叉树遍历方式来进行修改，序列化的结果是一个字符串，
+ * 序列化时通过 某种符号表示空节点（#），以 ！ 表示一个结点值的结束（value!）。
+ * <p>
+ * 二叉树的反序列化是指：根据某种遍历顺序得到的序列化字符串结果str，重构二叉树。
+ */
+
+public class Solution037 {
+
+    private String deserializeStr;
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        String serialize = new Solution037().Serialize(root);
+        System.out.println(serialize);
+    }
+
+    String Serialize(TreeNode root) {
+        if (root == null) {
+            return "#";
+        }
+        return root.val + " " + Serialize(root.left) + " " + Serialize(root.right);
+    }
+
+    TreeNode Deserialize(String str) {
+        deserializeStr = str;
+        return Deserialize();
+    }
+
+    private TreeNode Deserialize() {
+        if (deserializeStr.length() == 0) {
+            return null;
+        }
+        int index = deserializeStr.indexOf(" ");
+        String node = index == -1 ? deserializeStr : deserializeStr.substring(0, index);
+        deserializeStr = index == -1 ? "" : deserializeStr.substring(index + 1);
+        if (node.equals("#")) {
+            return null;
+        }
+        int val = Integer.parseInt(node);
+        TreeNode t = new TreeNode(val);
+        t.left = Deserialize();
+        t.right = Deserialize();
+        return t;
+    }
+
+}
