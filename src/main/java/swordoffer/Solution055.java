ac0b160d563cfcb700035d8b59ea8214ca05dd90
xiangflight <xiangflight@foxmail.com>
2020-01-03 22:21:08 +0800

    feat(swordoffer): solution 55

---
+++ src/main/java/swordoffer/Solution055.java
@@ -0,0 +1,41 @@
+package swordoffer;
+
+import ds.tree.TreeNode;
+
+/**
+ * @author xiangdotzhaoAtwoqutechcommacom
+ * @date 2020/1/3
+ * <p>
+ * 输入一棵二叉树，求该树的深度。从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。
+ */
+
+public class Solution055 {
+
+    private boolean isBalanced = true;
+
+    public int TreeDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = TreeDepth(root.left);
+        int right = TreeDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+
+    public boolean IsBalanced_Solution(TreeNode root) {
+        height(root);
+        return isBalanced;
+    }
+
+    private int height(TreeNode root) {
+        if (root == null || !isBalanced) {
+            return 0;
+        }
+        int left = height(root.left);
+        int right = height(root.right);
+        if (Math.abs(left - right) > 1) {
+            isBalanced = false;
+        }
+        return 1 + Math.max(left, right);
+    }
+}
