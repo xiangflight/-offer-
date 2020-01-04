8267c76e4291008f5307a2533b171b65e946a86b
xiangflight <xiangflight@foxmail.com>
2020-01-04 10:41:46 +0800

    feat(swordoffer): solution 58

---
+++ src/main/java/swordoffer/Solution058.java
@@ -0,0 +1,53 @@
+package swordoffer;
+
+/**
+ * @author xiangdotzhaoAtwoqutechcommacom
+ * @date 2020/1/3
+ *
+ * 牛客最近来了一个新员工Fish，每天早晨总是会拿着一本英文杂志，写些句子在本子上。
+ * 同事Cat对Fish写的内容颇感兴趣，有一天他向Fish借来翻看，但却读不懂它的意思。
+ * 例如，“student. a am I”。
+ * 后来才意识到，这家伙原来把句子单词的顺序翻转了，
+ * 正确的句子应该是“I am a student.”。Cat对一一的翻转这些单词顺序可不在行，你能帮助他么？
+ */
+
+public class Solution058 {
+
+    public String ReverseSentence(String str) {
+        int n = str.length();
+        char[] chars = str.toCharArray();
+        int i = 0, j = 0;
+        while (j <= n) {
+            if (j == n || chars[j] == ' ') {
+                reverse(chars, i, j - 1);
+                i = j + 1;
+            }
+            j++;
+        }
+        return new String(chars);
+    }
+
+    private void reverse(char[] chars, int i, int j) {
+        while (i < j) {
+            swap(chars, i++, j--);
+        }
+    }
+
+    private void swap(char[] arr, int i, int j) {
+        char tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public String LeftRotateString(String str,int n) {
+        if (str.length() <= n) {
+            return str;
+        }
+        char[] chars = str.toCharArray();
+        reverse(chars, 0, n - 1);
+        reverse(chars, n, chars.length - 1);
+        reverse(chars, 0, chars.length - 1);
+        return new String(chars);
+    }
+
+}
