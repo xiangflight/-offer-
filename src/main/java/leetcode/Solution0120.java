0598fd59146b69e5e7c86dffe68c39826124cc0e
xiangflight <xiangflight@foxmail.com>
2020-01-08 09:44:18 +0800

    feat(leetcode): Solution0120

--- src/main/java/leetcode/Solution0120.java
+++ src/main/java/leetcode/Solution0120.java
@@ -10,7 +10,20 @@ import java.util.List;
 public class Solution0120 {
 
     public int minimumTotal(List<List<Integer>> triangle) {
-
+        if (triangle == null || triangle.size() == 0) {
+            return 0;
+        }
+        int row = triangle.size();
+        int[] memo = new int[row];
+        for (int i = 0; i < triangle.get(row - 1).size(); i++) {
+            memo[i] = triangle.get(row - 1).get(i);
+        }
+        for (int layer = row - 2; layer >= 0; layer--) {
+            for (int j = 0; j <= layer; j++) {
+                memo[j] = Math.min(memo[j], memo[j+1]) + triangle.get(layer).get(j);
+            }
+        }
+        return memo[0];
     }
 
 }
