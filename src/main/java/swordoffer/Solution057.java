663d4312aa51ff2742842fa34a4a9c1d263fa7f7
xiangflight <xiangflight@foxmail.com>
2020-03-06 13:16:20 +0800

    refactor(swordoffer): add Solution057

--- src/main/java/swordoffer/Solution057.java
+++ src/main/java/swordoffer/Solution057.java
@@ -1,74 +1,73 @@
 package swordoffer;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author xiangdotzhaoAtwoqutechcommacom
  * @date 2020/1/3
+ * <p>
+ * 输入一个正整数 target ，输出所有和为 target 的连续正整数序列（至少含有两个数）。
+ * <p>
+ * 序列内的数字由小到大排列，不同序列按照首个数字从小到大排列。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：target = 9
+ * 输出：[[2,3,4],[4,5]]
+ * <p>
+ * ---------------------------------------------------------
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：target = 15
+ * 输出：[[1,2,3,4,5],[4,5,6],[7,8]]
  *
- * 输入一个递增排序的数组和一个数字S，在数组中查找两个数，使得他们的和正好是S，如果有多对数字的和等于S，输出两个数的乘积最小的。
- * 对应每个测试案例，输出两个数，小的先输出。
+ * S: Scenario
+ * A: Assumption
+ * E: Example
+ * T: Thought
+ * C: Code
+ * C: Complexity
+ * T: Test
+ *
+ * Limit: 1 <= target <= 10^5
+ *
  */
 
 public class Solution057 {
 
-    public ArrayList<Integer> FindNumbersWithSum(int [] array, int sum) {
-        if (array == null || array.length == 0) {
-            return new ArrayList<>();
-        }
-        int i = 0, j = array.length - 1;
-        while (i < j) {
-            int curSum = array[i] + array[j];
-            if (curSum == sum) {
-                return new ArrayList<>(Arrays.asList(array[i], array[j]));
-            }
-            if (curSum > sum) {
-                j--;
-            } else {
-                i++;
-            }
-        }
-        return new ArrayList<>();
-    }
-
     /**
-     * 输出所有和为 S 的连续正数序列。
+     * 使用不定长滑动窗口
      *
-     * 例如和为 100 的连续序列有：
-     *
-     * [9, 10, 11, 12, 13, 14, 15, 16]
-     * [18, 19, 20, 21, 22]。
-     *
-     * 输出所有和为S的连续正数序列。序列内按照从小至大的顺序，序列间按照开始数字从小到大的顺序
-     *
-     * @param sum
-     * @return
+     * @param target 窗口内值相加的和
+     * @return 所有满足条件的窗口
      */
-    public ArrayList<ArrayList<Integer> > FindContinuousSequence(int sum) {
-        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
-        int l = 1, r = 2;
-        int curSum = 3;
-        while (r < sum) {
-            if (curSum == sum) {
-                ArrayList<Integer> list = new ArrayList<>();
-                for (int i = l; i <= r; i++) {
-                    list.add(i);
+    public int[][] findContinuousSequence(int target) {
+        List<int[]> res = new ArrayList<>();
+        int lo = 1, hi = 2;
+        int sum = lo + hi;
+        while (hi < target) {
+            if (sum == target) {
+                int[] element = new int[hi - lo + 1];
+                for (int i = lo; i <= hi; i++) {
+                    element[i - lo] = i;
                 }
-                res.add(list);
-                curSum -= l;
-                l++;
-                r++;
-                curSum += r;
-            } else if (curSum < sum) {
-                r++;
-                curSum += r;
+                res.add(element);
+                // 整体右移
+                sum -= lo;
+                lo++;
+                hi++;
+                sum += hi;
+            } else if (sum < target) {
+                hi++;
+                sum += hi;
             } else {
-                curSum -= l;
-                l++;
+                sum -= lo;
+                lo++;
             }
         }
-        return res;
+        return res.toArray(new int[res.size()][]);
     }
 
 }
---
+++ src/test/java/swordoffer/Test057.java
@@ -0,0 +1,20 @@
+package swordoffer;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author xiangdotzhaoAtwoqutechcommacom
+ * @date 2020/3/6
+ */
+
+class Test057 {
+
+    private final Solution057 solution = new Solution057();
+
+    @Test
+    @DisplayName("Continuous Sequence")
+    void findContinuousSequence() {
+
+    }
+}
