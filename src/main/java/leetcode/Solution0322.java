4a0f6ee0584ab652261c4177cd8f770a45e98e79
xiangflight <xiangflight@foxmail.com>
2020-03-08 21:56:31 +0800

    refactor(swordoffer): add Solution322

--- src/main/java/leetcode/Solution0322.java
+++ src/main/java/leetcode/Solution0322.java
@@ -8,7 +8,31 @@ package leetcode;
 public class Solution0322 {
 
     public int coinChange(int[] coins, int amount) {
-        return 0;
+        if (amount < 1) {
+            return 0;
+        }
+        return coinChange(coins, amount, new int[amount]);
+    }
+
+    private int coinChange(int[] coins, int amount, int[] count) {
+        if (amount < 0) {
+            return -1;
+        }
+        if (amount == 0) {
+            return 0;
+        }
+        if (count[amount - 1] != 0) {
+            return count[amount - 1];
+        }
+        int min = Integer.MAX_VALUE;
+        for (int coin: coins) {
+            int ret = coinChange(coins, amount - coin, count);
+            if (ret >= 0 && ret < min) {
+                min = 1 + ret;
+            }
+        }
+        count[amount - 1] = (min == Integer.MAX_VALUE) ? -1: min;
+        return count[amount - 1];
     }
 
 }
