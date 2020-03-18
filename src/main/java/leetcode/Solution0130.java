c768ae1275e3fd43553d8409b62e55e12d8a51b5
xiangflight <xiangflight@foxmail.com>
2020-03-18 19:00:35 +0800

    feat(leetcode): Solution0130

--- src/main/java/leetcode/Solution0130.java
+++ src/main/java/leetcode/Solution0130.java
@@ -1,12 +1,66 @@
 package leetcode;
 
+import algo.uf.UF;
+
 /**
  * @author xiangdotzhaoAtwoqutechcommacom
  * @date 2019/12/21
+ *
+ * Union-Find 实现替换
  */
 
 public class Solution0130 {
 
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0) {
+            return;
+        }
+        int m = board.length;
+        int n = board[0].length;
+        UF uf = new UF(m * n + 1);
+        int dummy = m * n;
 
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') {
+                uf.union(i * n, dummy);
+            }
+            if (board[i][n - 1] == 'O') {
+                uf.union(i * n + n - 1, dummy);
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') {
+                uf.union(j, dummy);
+            }
+            if (board[m - 1][j] == 'O') {
+                uf.union(n * (m - 1) + j, dummy);
+            }
+        }
+
+        int[][] d = {{1, 0}, {0, 1}, {0, -1}, {-1, 0}};
+        for (int i = 1; i < m - 1; i++) {
+            for (int j = 1; j < n - 1; j++) {
+                if (board[i][j] == 'O') {
+                    for (int k = 0; k < 4; k++) {
+                        int x = i + d[k][0];
+                        int y = j + d[k][1];
+                        if (board[x][y] == 'O') {
+                            uf.union(x * n + y, i * n + j);
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i < m - 1; i++) {
+            for (int j = 1; j < n - 1; j++) {
+                if (!uf.connected(dummy, i * n + j)) {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+
+    }
 
 }
