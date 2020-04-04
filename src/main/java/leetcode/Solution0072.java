4c911c80987cab8eac80e9ca7cd3f13454762ec0
xiangflight <xiangflight@foxmail.com>
2020-04-04 21:53:48 +0800

    feat(leetcode): random

---
+++ src/main/java/leetcode/Solution0072.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+/**
+ * @author xiangdotzhaoAtwoqutechcommacom
+ * @date 2020/4/4
+ */
+
+public class Solution0072 {
+
+    public int minDistance(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        // base case
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    int min(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
+    }
+
+}
